1f61d9dd52aa3ab752fd9824558183f32f84648e
t-induwara <tharusha.induwar@gmail.com>
2023-08-14 22:38:24 +0530

    Added alert after completion of the process

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 const searchItems = [
     "acer",
+    "acer predator",
+    "react js",
     //If you want to add more items or need to change the list that suits for your preferences, feel free to add those here
 ];
 
@@ -7,28 +9,31 @@ var mainBtn = document.getElementById("btn");
 mainBtn.addEventListener("click",function(){
     mainBtn.classList.add("btn-clicked");
     
-    function openTab(url) {
-        window.open(url, '_blank');
-    }
-
-    async function performSearch(query) {
-        try {
-            const response = await fetch(`proxy.php?q=${encodeURIComponent(query)}`);
-            console.log(`Search query: ${query}, Status code: ${response.status}`);
-            
-            //This code open a new tab with the Bing search results
-            openTab(`https://www.bing.com/search?q=${encodeURIComponent(query)}`);
-        } catch (error) {
-            console.error(`Error performing search: ${error}`);
-        }
-    }
-
-    async function performSearchesWithDelay() {
-        for (const item of searchItems) {
-            await performSearch(item);
-            await new Promise(resolve => setTimeout(resolve, 3000)); //Delay between 2 tab openings for 3 seconds
-        }
-    }
-
     performSearchesWithDelay();
 })
+
+function openTab(url) {
+    window.open(url);
+}
+
+async function performSearch(query) {
+    try {
+        const response = await fetch(`proxy.php?q=${encodeURIComponent(query)}`);
+        console.log(`Search query: ${query}, Status code: ${response.status}`);
+        
+        //This code open a new tab with the Bing search results
+        openTab(`https://www.bing.com/search?q=${encodeURIComponent(query)}`);
+    } catch (error) {
+        console.error(`Error performing search: ${error}`);
+    }
+}
+
+async function performSearchesWithDelay() {
+    for (const item of searchItems) {
+        await performSearch(item);
+        await new Promise(resolve => setTimeout(resolve, 3000)); //Delay between 2 tab openings for 3 seconds
+    }
+    alert("Process completed!");
+}
+
+
